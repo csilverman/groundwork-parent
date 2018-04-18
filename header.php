27edea40fa0abcac165a8fd7981827b365304f55
csilverman <chris@csilverman.com>
2018-04-17 21:02:52 -0400

    Updated Google Analytics

--- header.php
+++ header.php
@@ -19,8 +19,7 @@ $html__classes = "";
 //	First, add post categories to the class list
 //	But only if we're on a post page
 
-
-if ( (is_single()) {
+if (is_single()) {
 	global $post;
 	$category = get_the_category($post->ID);
 	for ($i=0;$i<count($category);$i++) {
@@ -106,16 +105,17 @@ wp_reset_query();
 	</head>
 	
 	<body>
-		<script>
-		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 		
-		  ga('create', '<?php echo SITE__GOOGLEANALYTICS_CODE; ?>', 'auto');
-		  ga('send', 'pageview');
 		
-		</script>
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-59989151-4"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', '<?php echo SITE__GOOGLEANALYTICS_CODE; ?>');
+</script>
 		<div class="u-lPageContent hfeed site">
 
 		<?php if(SITE__HAS_NAVIGATION) { ?>
